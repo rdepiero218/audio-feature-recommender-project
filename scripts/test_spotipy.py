ebaeea24f6f1ac8eb7f6429ab9d59f1a191c477b
Regene DePiero <regenedepiero@gmail.com>
2020-11-05 11:44:56 -0500

    removed Spotify authorization IDs from files

--- scripts/test_spotipy.py
+++ scripts/test_spotipy.py
@@ -1,11 +1,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-clientID = 'c85f43a5a7004946996bc22659dd8de6'
-clientSecret = '9f6e5b2d26af429ba2c3a77624f85a2d'
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientID,
-                                                           client_secret=clientSecret))
+### Spotify Credentials - must be set in local environment to run
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 results = sp.search(q='weezer', limit=20)
 
